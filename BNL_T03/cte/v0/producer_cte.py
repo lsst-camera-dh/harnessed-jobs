01b447da611f51f473dab8c15bace39d058118ce
Jim Chiang <jchiang@slac.stanford.edu>
2015-04-26 18:15:18 -0700

    add cte scripts

---
+++ BNL_T03/cte/v0/producer_cte.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import sys
+import lsst.eotest.sensor as sensorTest
+from lcatr.harness.helpers import dependency_glob
+import siteUtils
+
+superflat_files = dependency_glob('*_superflat*.fits', jobname='sflat_acq')
+mask_files = dependency_glob('*_mask.fits')
+
+print superflat_files
+print mask_files
+sys.stdout.flush()
+
+sensor_id = siteUtils.getUnitId()
+
+task = sensorTest.CteTask()
+task.run(sensor_id, superflat_files, mask_files)
