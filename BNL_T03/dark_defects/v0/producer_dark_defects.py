797e6ba425d27b84d8fee254d23ea575b9fea309
Jim Chiang <jchiang@slac.stanford.edu>
2015-04-26 17:37:55 -0700

    add dark_defects scripts

---
+++ BNL_T03/dark_defects/v0/producer_dark_defects.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import sys
+import lsst.eotest.sensor as sensorTest
+from lcatr.harness.helpers import dependency_glob
+import siteUtils
+
+sflat_files = dependency_glob('*_superflat_500_*.fits', jobname='sflat_acq')
+mask_files = dependency_glob('*_mask.fits')
+
+print sflat_files
+print mask_files
+sys.stdout.flush()
+
+sensor_id = siteUtils.getJobId()
+
+task = sensorTest.DarkPixelsTask()
+task.run(sensor_id, sflat_files, mask_files)
--- BNL_T03/dark_defects/v0/validator_dark_defects.py
+++ BNL_T03/dark_defects/v0/validator_dark_defects.py
@@ -3,21 +3,21 @@ import lsst.eotest.sensor as sensorTest
 import lcatr.schema
 import siteUtils
 
-sensor_id = siteUtils.getUnitId()
+sensor_id = siteUtils.getJobId()
 
-mask_file = '%s_bright_pixel_mask.fits' % sensor_id
+mask_file = '%s_dark_pixel_mask.fits' % sensor_id
 results = [lcatr.schema.fileref.make(mask_file)]
 
 eotest_results = '%s_eotest_results.fits' % sensor_id
 data = sensorTest.EOTestResults(eotest_results)
 amps = data['AMP']
-npixels = data['NUM_BRIGHT_PIXELS']
-ncolumns = data['NUM_BRIGHT_COLUMNS']
+npixels = data['NUM_DARK_PIXELS']
+ncolumns = data['NUM_DARK_COLUMNS']
 for amp, npix, ncol in zip(amps, npixels, ncolumns):
-    results.append(lcatr.schema.valid(lcatr.schema.get('bright_defects'),
+    results.append(lcatr.schema.valid(lcatr.schema.get('dark_defects'),
                                       amp=amp,
-                                      bright_pixels=npix,
-                                      bright_columns=ncol))
+                                      dark_pixels=npix,
+                                      dark_columns=ncol))
 
 lcatr.schema.write_file(results)
 lcatr.schema.validate_file()
