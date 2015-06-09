8eb26378df7c6ac1e86176e5c61d6598733418b0
Homer A. Neal Jr <homer@rhel6-64i.slac.stanford.edu>
2015-06-09 08:33:15 -0700

    fixed

--- BNL_T03/mono_calib_acq/v0/ccs_mono_calib.py
+++ BNL_T03/mono_calib_acq/v0/ccs_mono_calib.py
@@ -50,7 +50,7 @@ try:
     reply = result.getResult();
     time.sleep(3.);
 
-  print "Throwing away the first image"
+   print "Throwing away the first image"
     arcsub.synchCommand(10,"setFitsFilename","");
     result = arcsub.synchCommand(200,"exposeAcquireAndSave");
     reply = result.getResult();
@@ -145,70 +145,70 @@ try:
 
 
 # take light exposures
-            arcsub.synchCommand(10,"setParameter","Light","1");
-            arcsub.synchCommand(10,"setParameter","ExpTime",str(int(exptime*1000)));
-            print "setting location of fits exposure directory"
-            arcsub.synchCommand(10,"setFitsDirectory","%s" % (cdir));
+        arcsub.synchCommand(10,"setParameter","Light","1");
+        arcsub.synchCommand(10,"setParameter","ExpTime",str(int(exptime*1000)));
+        print "setting location of fits exposure directory"
+        arcsub.synchCommand(10,"setFitsDirectory","%s" % (cdir));
 
 # prepare to readout diodes
-            nreads = exptime*60/nplc + 200
-            if (nreads > 3000):
-                nreads = 3000
-                nplc = exptime*60/(nreads-200)
-                print "Nreads limited to 3000. nplc set to %f to cover full exposure period " % nplc
+        nreads = exptime*60/nplc + 200
+        if (nreads > 3000):
+            nreads = 3000
+            nplc = exptime*60/(nreads-200)
+            print "Nreads limited to 3000. nplc set to %f to cover full exposure period " % nplc
 
-            for i in range(1,1):
-                print "starting acquisition step for lambda = %8.2f" % wl
+        for i in range(1,1):
+            print "starting acquisition step for lambda = %8.2f" % wl
 
-                monosub.synchCommand(30,"setWaveAndFilter",wl);
+            monosub.synchCommand(30,"setWaveAndFilter",wl);
 
 # adjust timeout because we will be waiting for the data to become ready
-                mywait = nplc/60.*nreads*1.10 ;
-                print "Setting timeout to %f s" % mywait
-                pdsub.synchCommand(1000,"setTimeout",mywait);
+            mywait = nplc/60.*nreads*1.10 ;
+            print "Setting timeout to %f s" % mywait
+            pdsub.synchCommand(1000,"setTimeout",mywait);
 
-                print "call accumBuffer to start PD recording at %f" % time.time()
-                pdresult =  pdsub.asynchCommand("accumBuffer",int(nreads),float(nplc),True);
+            print "call accumBuffer to start PD recording at %f" % time.time()
+            pdresult =  pdsub.asynchCommand("accumBuffer",int(nreads),float(nplc),True);
 
-                print "recording should now be in progress and the time is %f" % time.time()
+            print "recording should now be in progress and the time is %f" % time.time()
 
 # start acquisition
-                timestamp = time.time()
-                fitsfilename = "%s_lambda_%3.3d_%3.3d_lambda_%d_${TIMESTAMP}.fits" % ("mono_calib",int(wl),seq,i+1)
-                arcsub.synchCommand(10,"setFitsFilename",fitsfilename);
+            timestamp = time.time()
+            fitsfilename = "%s_lambda_%3.3d_%3.3d_lambda_%d_${TIMESTAMP}.fits" % ("mono_calib",int(wl),seq,i+1)
+            arcsub.synchCommand(10,"setFitsFilename",fitsfilename);
 
 # make sure to get some readings before the state of the shutter changes       
-                time.sleep(0.2);
+            time.sleep(0.2);
  
 
-                print "Ready to take image. time = %f" % time.time()
-                result = arcsub.synchCommand(200,"exposeAcquireAndSave");
-                fitsfilename = result.getResult();
-                print "after click click at %f" % time.time()
+            print "Ready to take image. time = %f" % time.time()
+            result = arcsub.synchCommand(200,"exposeAcquireAndSave");
+            fitsfilename = result.getResult();
+            print "after click click at %f" % time.time()
 
-                print "done with exposure # %d" % i
-                print "getting photodiode readings at time = %f" % time.time();
+            print "done with exposure # %d" % i
+            print "getting photodiode readings at time = %f" % time.time();
 
-                pdfilename = "pd-values_%d-for-seq-%d-exp-%d.txt" % (int(timestamp),seq,i+1)
+            pdfilename = "pd-values_%d-for-seq-%d-exp-%d.txt" % (int(timestamp),seq,i+1)
 # the primary purpose of this is to guarantee that the accumBuffer method has completed
-                print "starting the wait for an accumBuffer done status message at %f" % time.time()
-                tottime = pdresult.get();
+            print "starting the wait for an accumBuffer done status message at %f" % time.time()
+            tottime = pdresult.get();
 
 # make sure the sample of the photo diode is complete
-                time.sleep(10.)
+            time.sleep(10.)
 
-                print "executing readBuffer, cdir=%s , pdfilename = %s" % (cdir,pdfilename)
-                result = pdsub.synchCommand(1000,"readBuffer","%s/%s" % (cdir,pdfilename));
-                buff = result.getResult()
-                print "Finished getting readings at %f" % time.time()
+            print "executing readBuffer, cdir=%s , pdfilename = %s" % (cdir,pdfilename)
+            result = pdsub.synchCommand(1000,"readBuffer","%s/%s" % (cdir,pdfilename));
+            buff = result.getResult()
+            print "Finished getting readings at %f" % time.time()
 
 # reset timeout to something reasonable for a regular command
-                pdsub.synchCommand(1000,"setTimeout",10.);
+            pdsub.synchCommand(1000,"setTimeout",10.);
 
 
-                fpfiles.write("%s %s/%s %f\n" % (fitsfilename,cdir,pdfilename,timestamp))
+            fpfiles.write("%s %s/%s %f\n" % (fitsfilename,cdir,pdfilename,timestamp))
 
-            seq = seq + 1
+        seq = seq + 1
 
     fpfiles.close();
     fp.close();
