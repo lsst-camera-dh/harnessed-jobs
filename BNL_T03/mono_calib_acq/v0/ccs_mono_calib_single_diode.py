04880da7b265248ef049eec018e002cf35adc12d
Homer A. Neal Jr <homer@rhel6-64k.slac.stanford.edu>
2015-06-11 05:32:16 -0700

    add readout of the 2nd diode

--- BNL_T03/mono_calib_acq/v0/ccs_mono_calib_single_diode.py
+++ BNL_T03/mono_calib_acq/v0/ccs_mono_calib_single_diode.py
@@ -34,6 +34,11 @@ try:
 # Initialization
     print "doing initialization"
 
+    print "resetting PD device"
+#    result = pdsub.synchCommand(20,"reset")
+#    reply = result.getResult();
+#    time.sleep(5.)
+
     print "Loading configuration file into the Archon controller"
     result = arcsub.synchCommand(20,"setConfigFromFile",acffile);
     reply = result.getResult();
@@ -45,10 +50,10 @@ try:
     reply = result.getResult();
     time.sleep(3.);
 
-#    print "Throwing away the first image"
-#    arcsub.synchCommand(10,"setFitsFilename","");
-#    result = arcsub.synchCommand(200,"exposeAcquireAndSave");
-#    reply = result.getResult();
+    print "Throwing away the first image"
+    arcsub.synchCommand(10,"setFitsFilename","");
+    result = arcsub.synchCommand(200,"exposeAcquireAndSave");
+    reply = result.getResult();
 
 #    arcsub.synchCommand(10,"setAcqParam","Nexpo");
     arcsub.synchCommand(10,"setParameter","Expo","1");
@@ -61,6 +66,27 @@ try:
     result = tssub.synchCommand(60,"setTSTEST");
     rply = result.getResult();
 
+#check state of ts devices
+    print "wait for ts state to become ready";
+    tsstate = 0
+    starttim = time.time()
+    while True:
+        print "checking for test stand to be ready for acq";
+        result = tssub.synchCommand(10,"isTestStandReady");
+        tsstate = result.getResult();
+# the following line is just for test situations so that there would be no waiting
+        tsstate=1;
+        if ((time.time()-starttim)>240):
+            print "Something is wrong ... we will never make it to a runnable state"
+            exit
+        if tsstate!=0 :
+            break
+        time.sleep(5.)
+
+#put in acquisition state
+#    print "go teststand go"
+#    result = tssub.synchCommand(120,"goTestStand");
+#    rply = result.getResult();
 
 # get the glowing vacuum gauge off
 #    result = pdusub.synchCommand(120,"setOutletState",vac_outlet,False);
@@ -88,8 +114,35 @@ try:
     fpfiles = open("%s/acqfilelist" % cdir,"w");
 
     for wl in range(300,310,5):
+#     for line in fp:
+#        tokens = str.split(line)
+#        if ((len(tokens) > 0) and (tokens[0] == 'lambda')):
+#            wl = int(tokens[1])
+#            target = float(tokens[1])
+#            print "target wl = %f" % target;
 
         exptime = 12.
+#            exptime = eolib.expCheck(calfile, labname, target, wl, hi_lim, lo_lim, test='LAMBDA', use_nd=False)
+
+
+# take bias images
+
+#            arcsub.synchCommand(10,"setParameter","ExpTime","0"); 
+#            arcsub.synchCommand(10,"setParameter","Light","0");
+
+#            print "setting location of bias fits directory"
+#            arcsub.synchCommand(10,"setFitsDirectory","%s" % (cdir));
+
+#           for i in range(bcount):
+#               timestamp = time.time()
+#               fitsfilename = "%s_lambda_bias_%3.3d_${TIMESTAMP}.fits" % (ccd,seq)
+#               arcsub.synchCommand(10,"setFitsFilename",fitsfilename);
+
+#               print "Ready to take bias image. time = %f" % time.time()
+#               result = arcsub.synchCommand(200,"exposeAcquireAndSave");
+#               fitsfilename = result.getResult();
+#               print "after click click at %f" % time.time()
+#               time.sleep(0.2)
 
 
 # take light exposures
@@ -98,6 +151,12 @@ try:
         print "setting location of fits exposure directory"
         arcsub.synchCommand(10,"setFitsDirectory","%s" % (cdir));
 
+# prepare to readout diodes
+#        nreads = exptime*60/nplc + 200
+#        if (nreads > 3000):
+#            nreads = 3000
+#            nplc = exptime*60/(nreads-200)
+#            print "Nreads limited to 3000. nplc set to %f to cover full exposure period " % nplc
         nreads = 3000
 
         for i in range(1,2):
@@ -108,24 +167,12 @@ try:
 # adjust timeout because we will be waiting for the data to become ready
             mywait = nplc/60.*nreads*1.10 ;
             print "Setting timeout to %f s" % mywait
-
-# PD from PD setup
-            print "Setup regular PD readout"
             pdsub.synchCommand(1000,"setTimeout",mywait);
 
             print "call accumBuffer to start PD recording at %f" % time.time()
             pdresult =  pdsub.asynchCommand("accumBuffer",int(nreads),float(nplc),True);
 
-            print "PD recording from PD device should now be in progress and the time is %f" % time.time()
-
-# PD from BIAS setup
-            print "Setup PD readout from Bias device"
-            biassub.synchCommand(1000,"setTimeout",mywait);
-
-            print "call accumBuffer to start PD recording from Bias device at %f" % time.time()
-            pdbiasresult =  biassub.asynchCommand("accumBuffer",int(nreads),float(nplc),True);
-
-            print "PD recording from Bias device should now be in progress and the time is %f" % time.time()
+            print "recording should now be in progress and the time is %f" % time.time()
 
 # start acquisition
             timestamp = time.time()
@@ -142,8 +189,7 @@ try:
             print "after click click at %f" % time.time()
 
             print "done with exposure # %d" % i
-# readings from PD device
-            print "getting photodiode readings from PD device at time = %f" % time.time();
+            print "getting photodiode readings at time = %f" % time.time();
 
             pdfilename = "pd-values_%d-for-seq-%d-exp-%d.txt" % (int(timestamp),seq,i+1)
 # the primary purpose of this is to guarantee that the accumBuffer method has completed
@@ -158,28 +204,11 @@ try:
             buff = result.getResult()
             print "Finished getting readings at %f" % time.time()
 
-# readings from PD device
-            print "getting photodiode readings from Bias device at time = %f" % time.time();
-
-            pdbiasfilename = "pdbias-values_%d-for-seq-%d-exp-%d.txt" % (int(timestamp),seq,i+1)
-# the primary purpose of this is to guarantee that the accumBuffer method has completed
-            print "starting the wait for an accumBuffer done status message at %f" % time.time()
-            tottime = pdbiasresult.get();
-
-# make sure the sample of the photo diode is complete
-            time.sleep(10.)
-
-            print "executing readBuffer, cdir=%s , pdfilename = %s" % (cdir,pdbiasfilename)
-            result = pdbiassub.synchCommand(1000,"readBuffer","%s/%s" % (cdir,pdbiasfilename));
-            buff = result.getResult()
-            print "Finished getting readings at %f" % time.time()
-
 # reset timeout to something reasonable for a regular command
             pdsub.synchCommand(1000,"setTimeout",10.);
-            pdbiassub.synchCommand(1000,"setTimeout",10.);
 
 
-            fpfiles.write("%s %s/%s %s/%s %f\n" % (fitsfilename,cdir,pdfilename,cdir,pdbiasfilename,timestamp))
+            fpfiles.write("%s %s/%s %f\n" % (fitsfilename,cdir,pdfilename,timestamp))
 
         seq = seq + 1
 
