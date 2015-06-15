7c54fc0e178605d9ad2e93d0c6c608e4cdd99eee
Jim Chiang <jchiang@slac.stanford.edu>
2015-06-15 09:37:09 -0700

    use updated interface to eotest qeTask.py to use the new BNL pd ratio file format

--- BNL_T03/qe_analysis/v0/producer_qe_analysis.py
+++ BNL_T03/qe_analysis/v0/producer_qe_analysis.py
@@ -8,14 +8,12 @@ sensor_id = siteUtils.getUnitId()
 lambda_files = siteUtils.dependency_glob('*_lambda_*.fits',
                                          jobname=siteUtils.getProcessName('qe_acq'),
                                          description='Lambda files:')
+# The photodiode ratio file for BNL data.
+# @todo Replace with ratio file for production work when it is available.
+pd_ratio_file = os.path.join(os.environ['EOTEST_DIR'], 'data', 'qe',
+                             'BNL', 'pd_Cal_mar2013_v1.txt')
 mask_files = eotestUtils.glob_mask_files()
 gains = eotestUtils.getSensorGains()
 
-# The photodiode ratio file for BNL data.
-# @todo Replace with ratio file for production work when it is available.
-pd_cal_file = os.path.join(os.environ['EOTEST_DIR'], 'data', 'qe',
-                           'BNL', 'pd_Cal_mar2013.txt')
-
 task = sensorTest.QeTask()
-task.run(sensor_id, lambda_files, None, None, None, mask_files, gains,
-         pd_cal_file=pd_cal_file)
+task.run(sensor_id, lambda_files, pd_ratio_file, mask_files, gains)
