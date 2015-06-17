e07b46ae986a025443a18ab98e52f6ac88086d02
Homer A. Neal Jr <homer@rhel6-64f.slac.stanford.edu>
2015-06-16 17:43:34 -0700

    typo fixed

--- python/hdrtools.py
+++ python/hdrtools.py
@@ -304,10 +304,10 @@ def fitsExptime(filename):
 def updateFitsHeaders(acqfilelist, summaryFile="summary.txt"):
     for line in open(acqfilelist):
         nelem = len(line.split())
-#       if (nelem==3):
-#           fitsfile, pdfile, tstamp = line.split()[:3]
-#       else:
-#           fitsfile, pdfile, bsfile, tstamp = line.split()[:4]
+        if (nelem==3):
+            fitsfile, pdfile, tstamp = line.split()[:3]
+        else:
+            fitsfile, pdfile, bsfile, tstamp = line.split()[:4]
 #       try:
 #           addPDvals(fitsfile, pdfile, "AMP0.MEAS_TIMES", "AMP0", tstamp)
 #       except:
