131583535ae752230b3c7903f3525431771ebb99
Homer A. Neal Jr <homer@rhel6-64f.slac.stanford.edu>
2015-07-05 16:20:39 -0700

    fixed typos

--- BNL_T03/Setup_EO_Scripts/v0/producer_Setup_EO_Scripts.py
+++ BNL_T03/Setup_EO_Scripts/v0/producer_Setup_EO_Scripts.py
@@ -8,9 +8,9 @@ tag = os.environ["EO_SCRIPTS_TAG"]
 eohome = os.environ["EO_SCRIPTS_HOME"]
 
 
-print "The realease of harnessed-jobs with tag %s will be installed" % tag
+print "The release of harnessed-jobs with tag %s will be installed" % tag
  
-os.system("cd %s ; wget https://github.com/lsst-camera-dh/harnessed-jobs/archive/%s.tar.gz ." % (eohome,tag))
+os.system("cd %s ; wget https://github.com/lsst-camera-dh/harnessed-jobs/archive/%s.tar.gz" % (eohome,tag))
 os.system("cd %s ; tar -vzxf %s.tar.gz" % (eohome,tag))
 os.system("cd %s ; mv harnessed-jobs old-harnessed-jobs-`date +%%R-%%f`" % eohome)
 os.system("cd %s ; ln -s harnessed-jobs-%s harnessed-jobs" % (eohome,tag))
--- BNL_T03/Setup_EO_Scripts/v0/validator_Setup_EO_Scripts.py
+++ BNL_T03/Setup_EO_Scripts/v0/validator_Setup_EO_Scripts.py
@@ -4,13 +4,3 @@ import lcatr.schema
 import os
     
 results = []
-
-os.system("cp -vp ~/Desktop/hardwareReceipt/* .")
-os.system("chmod 644 *.*")
-
-files = glob.glob('*.*')    
-data_products = [lcatr.schema.fileref.make(item) for item in files]
-results.extend(data_products)
-
-lcatr.schema.write_file(results)
-lcatr.schema.validate_file()
