034029c8f82b0f1aef349cb703873c7ac459bc3f
Homer A. Neal Jr <homer@rhel6-64f.slac.stanford.edu>
2015-07-07 16:57:28 -0700

    fixed

--- BNL_T03/Setup_EO_Scripts/v0/validator_Setup_EO_Scripts.py
+++ BNL_T03/Setup_EO_Scripts/v0/validator_Setup_EO_Scripts.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 import glob
+import lsst.eotest.sensor as sensorTest
 import lcatr.schema
-import os
-    
+import siteUtils
+
+sensor_id = siteUtils.getUnitId()
+
 results = []
+
+results.append(siteUtils.packageVersions())
+
+lcatr.schema.write_file(results)
+lcatr.schema.validate_file()
