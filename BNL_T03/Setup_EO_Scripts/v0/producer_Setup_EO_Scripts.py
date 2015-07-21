4d5d64f70e9f7de2a843691b11fb66d5eb176dcc
Homer A. Neal Jr <homer@rhel6-64a.slac.stanford.edu>
2015-07-21 09:22:45 -0700

    added -L option to cp for dereferencing

--- BNL_T03/Setup_EO_Scripts/v0/producer_Setup_EO_Scripts.py
+++ BNL_T03/Setup_EO_Scripts/v0/producer_Setup_EO_Scripts.py
@@ -11,9 +11,9 @@ eohome = os.environ["EO_SCRIPTS_HOME"]
 print "The release of harnessed-jobs with tag %s will be installed" % tag
 cwd = os.getcwd() 
 print "copying old installation of harnessed jobs to a safe place"
-os.system("cd %s ; cp -rp harnessed-jobs old-harnessed-jobs-`date +%%F-%%R`" % eohome)
+os.system("cd %s ; cp -Lrvp harnessed-jobs old-harnessed-jobs-`date +%%F-%%R`" % eohome)
 print "moving the original to /tmp"
-os.system("cd %s ; mv harnessed-jobs /tmp/" % eohome)
+os.system("cd %s ; cp -Lvrp harnessed-jobs /tmp/ ; mv harnessed-jobs moved-harnessed-jobs" % eohome)
 print "downloading tar of new tag"
 os.system("cd %s ; wget https://github.com/lsst-camera-dh/harnessed-jobs/archive/%s.tar.gz" % (eohome,tag))
 print "untarring"
