441aa7449dd89fdce098592f3a46a5aed0e9b57c
Jim Chiang <jchiang@slac.stanford.edu>
2015-07-29 13:11:53 -0700

    bug-fix in producer_cte*.py scripts (passing mask files as bias files); write total number of pixels and masked pixels to eotest results file header in producer_test_report*.py scripts; set gain_error in fe55_analysis in validator_vendorIngest.py; use high flux superflats in vendorFitsTranslators.py

--- BNL_T03/cte/v0/producer_cte.py
+++ BNL_T03/cte/v0/producer_cte.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 import lsst.eotest.sensor as sensorTest
 import siteUtils
-import eotestUtils
 
 sensor_id = siteUtils.getUnitId()
 sflat_files = siteUtils.dependency_glob('*_sflat_500*.fits',
                                         jobname=siteUtils.getProcessName('sflat_acq'),
                                         description='Superflat files:')
-mask_files = eotestUtils.glob_mask_files()
 
 task = sensorTest.CteTask()
-task.run(sensor_id, sflat_files, mask_files)
+task.run(sensor_id, sflat_files)
