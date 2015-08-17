4672bad62c794ddcf7b217a13565554bf556467d
Homer Neal <homerneal@yahoo.fr>
2015-08-17 05:58:21 -0700

    Update producer_Setup-InJig-Metrology-Directories-and-Scripts.py
    intermediate version

--- BNL_T03/Setup-InJig-Metrology-Directories-and-Scripts/v0/producer_Setup-InJig-Metrology-Directories-and-Scripts.py
+++ BNL_T03/Setup-InJig-Metrology-Directories-and-Scripts/v0/producer_Setup-InJig-Metrology-Directories-and-Scripts.py
@@ -26,8 +26,10 @@ print "Creating dated edge directory for the CCD at %s" % flatdatedir
 os.makedirs(flatdatedir)
 print "Please setup the OGP MeasureMind application to store results in respective edge scan and flatness directories indicated above"
 # leave a link to the location where the files should go
+print "Making links to the data directories in %s" % os.getcwd()
 os.system("ln -s %s edgelink" % edgedatedir);
 os.system("ln -s %s flatlink" % flatdatedir);
+os.system("ls -lrt")
 
 #top = Tkinter.Tk()
 #M = Tkinter.Button(top, text ="Please setup the OGP MeasureMind application to store<br>edge scan results in %s edgedatedir<br>and<br>flatness results in %s" % (edgedatedir,flatdatedir), bg = "green")
@@ -36,3 +38,25 @@ os.system("ln -s %s flatlink" % flatdatedir);
 #top.title('OGP Dirs Ready')
 #top.mainloop()
 print "Please setup the OGP MeasureMind application to store\nedge scan results in %s edgedatedir\nand\nflatness results in %s" % (edgedatedir,flatdatedir)
+print "============================================="
+print "Now installing scripts"
+tag = os.environ["OGP_SCRIPTS_TAG"]
+eohome = os.environ["OGP_SCRIPTS_HOME"]
+
+
+print "The release of OGP-scripts with tag %s will be installed" % tag
+cwd = os.getcwd() 
+print "copying old installation of harnessed jobs to a safe place"
+os.system("cd %s ; cp -Lrvp harnessed-jobs old-harnessed-jobs-`date +%%F-%%R`" % eohome)
+print "moving the original to /tmp"
+os.system("cd %s ; cp -Lvrp harnessed-jobs /tmp/ ; mv harnessed-jobs moved-harnessed-jobs" % eohome)
+print "downloading tar of new tag"
+os.system("cd %s ; wget https://github.com/lsst-camera-dh/harnessed-jobs/archive/%s.tar.gz" % (eohome,tag))
+print "untarring"
+os.system("cd %s ; tar -vzxf %s.tar.gz" % (eohome,tag))
+print "making a link to it"
+os.system("cd %s ; ln -s harnessed-jobs-%s harnessed-jobs" % (eohome,tag))
+
+os.system("cd %s" % cwd)
+
+print "The release has been installed."
