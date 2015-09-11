44aa512d573059055be2093937b40890e940ceca
Jim Chiang <jchiang@slac.stanford.edu>
2015-09-11 11:13:16 -0700

    enable 1% statistical accuracy criterion for Fe55 gain fits

--- BNL_T03/fe55_analysis/v0/producer_fe55_analysis.py
+++ BNL_T03/fe55_analysis/v0/producer_fe55_analysis.py
@@ -14,4 +14,4 @@ rolloff_mask_file = '%s_rolloff_defects_mask.fits' % sensor_id
 sensorTest.rolloff_mask(fe55_files[0], rolloff_mask_file)
 
 task = sensorTest.Fe55Task()
-task.run(sensor_id, fe55_files, (rolloff_mask_file,))
+task.run(sensor_id, fe55_files, (rolloff_mask_file,), accuracy_req=0.01)
