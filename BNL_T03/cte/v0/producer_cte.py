be08524bf9fe855bb9c3d1d3f563914bd19a41c2
Jim Chiang <jchiang@slac.stanford.edu>
2015-09-14 18:03:30 -0700

    use correction image

--- BNL_T03/cte/v0/producer_cte.py
+++ BNL_T03/cte/v0/producer_cte.py
@@ -3,7 +3,7 @@ import lsst.eotest.sensor as sensorTest
 import siteUtils
 
 sensor_id = siteUtils.getUnitId()
-sflat_files = siteUtils.dependency_glob('*_sflat_500*.fits',
+sflat_files = siteUtils.dependency_glob('*_sflat_*500*.fits',
                                         jobname=siteUtils.getProcessName('sflat_acq'),
                                         description='Superflat files:')
 
