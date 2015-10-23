b96d055e70022439f9876f8d593960f4095fd23b
richardxdubois <richard.x.dubois@gmail.com>
2015-10-23 15:32:44 -0700

    update for command line args and finding latest activity id. Still need to handle test name better

--- python/DataCatalog.py
+++ python/DataCatalog.py
@@ -6,8 +6,49 @@ import os
 import subprocess
 import datacat
 import datacat.error
+from datacat import client_from_config_file
+from datacat.model import Folder
+import argparse
 
 remote_hosts = {'SLAC' : 'rhel6-64.slac.stanford.edu'}
+
+## Command line arguments
+parser = argparse.ArgumentParser(description='Find archived data in the LSST  data Catalog. These include CCD test stand and vendor data files.')
+
+## FT1 metadata filter opts
+parser.add_argument('-f','--filter',default=None,help="FT1 metadata filter string (default=%(default)s)")
+##   The following are 'convenience options' which could also be specified in the filter string
+parser.add_argument('-t','--timestamp',default=None,help="(metadata) File timestamp (default=%(default)s)")
+parser.add_argument('-s','--sensorID', default=None,help="(metadata) Sensor ID (default=%(default)s)")
+parser.add_argument('-T','--TestName', default=None,help="(metadata) test type (default=%(default)s)")
+parser.add_argument('-c','--CCDType', default="ITL",help="(metadata) CCD vendor type (default=%(default)s)")
+parser.add_argument('-S','--site', default="SLAC",help="File location (default=%(default)s) ")
+
+## Limit dataCatalog search to specified parts of the catalog
+parser.add_argument('-g','--group',default=None,help="Limit search to specified dataCat group (default=%(default)s)")
+parser.add_argument('-m','--mirrorName',default='BNL-test',help="mirror name to search, i.e. in dataCat /LSST/mirror/<mirrorName> (default=%(default)s)")
+parser.add_argument('-X','--XtraOpts',default='',help="any extra 'datacat find' options (default=%(default)s)")
+
+## Output
+parser.add_argument('-o','--outputFile',default=None,help="Output result to specified file (default = %(default)s)")
+parser.add_argument('-a','--displayAll',default=False,action='store_true',help="Display entire result set (default = %(default)s)")
+
+## Verbosity
+parser.add_argument('-d','--debug',default=False,action='store_true',help="enable debug mode (default=%(default)s)")
+parser.add_argument('-x','--dryRun',default=False,action='store_true',help="dry run (no DB action) (default=%(default)s)")
+
+args = parser.parse_args()
+
+client = client_from_config_file()
+
+def is_valid_folder(child):
+    try:
+        if isinstance(child, Folder)  and float(child.name):
+            return True
+    except ValueError:
+        pass
+    return False
+
 
 def _get_job_id(dataset):
     folder = os.path.split(dataset.path)[0]
@@ -150,20 +191,66 @@ class DataCatalog(object):
         return DatasetList(resp, self, job_id=job_id, job_name=job_name)
 
 if __name__ == '__main__':
-    folder = '/LSST/mirror/BNL3'
-    query = 'TEMP_SET==-125 && TESTTYPE="DARK"'
-    site = 'SLAC'
 
-    datacatalog = DataCatalog(folder=folder, experiment='LSST', site=site)
+    sensorID = args.sensorID
+
+    sourceMap = {
+        'BNL-prod': 'BNL-prod/prod/',
+        'BNL-test': 'BNL-test/test/',
+        'vendorCopy-prod': 'SLAC-prod/prod/',
+        'vendorCopy-test': 'SLAC-test/test/',
+        'vendor-prod': 'vendorData/',
+        'vendor-test': 'vendorData/'
+        }
+    
+    folder = '/LSST/'  
+
+    if (args.mirrorName == 'BNL-prod' or args.mirrorName == 'BNL-test'):
+        folder = folder + 'mirror/' + sourceMap[args.mirrorName] + args.CCDType + '-CCD/' + sensorID + '/' + args.TestName + '/'
+    elif (args.mirrorName == 'vendorCopy-prod' or args.mirrorName == 'vendorCopy-test'):
+        folder = folder + '/mirror/' + sourceMap[args.mirrorName] + args.CCDType + '-CCD/' + sensorID + '/vendorIngest/'
+    elif (args.mirrorName == 'vendor-prod'):
+        folder = folder + sourceMap[args.mirrorName] + args.CCDType  + '/' + sensorID + '/Prod/'
+    elif (args.mirrorName == 'vendor-test'):
+        folder = folder + sourceMap[args.mirrorName] + args.CCDType  + '/' + sensorID + '/Dev/'
+        
+
+    print folder
+    children = [c for c in client.children(folder) if is_valid_folder(c)]
+
+    nkey = lambda c: float(c.name)
+    children.sort(key=nkey)
+
+    latest_path = children[-1].path
+    print latest_path
+        
+    query = args.XtraOpts
+    site = args.site
+
+    datacatalog = DataCatalog(folder=latest_path, experiment='LSST', site=site)
 
     datasets = datacatalog.find_datasets(query)
     print "%i datasets found\n" % len(datasets)
 
-    nfiles = 5
-    print "File paths for first %i files at %s:" % (nfiles, site)
-    for item in datasets.full_paths()[:nfiles]:
-        print item
+    nfiles = 30
+    if args.debug:
+        print "File paths for first %i files at %s:" % (nfiles, site)
+        for item in datasets.full_paths()[:nfiles]:
+            print item
 
     print
 
-    datasets.download(dryrun=True, clobber=False, nfiles=nfiles)
+    ## Write file with list of found data files, if requested
+    if args.outputFile != None and len(datasets)>0:
+        print 'Writing output file ',args.outputFile,'...'
+        ofile = open(args.outputFile,'w')
+        for line in datasets.full_paths():
+            ofile.write(line+'\n')
+            pass
+        ofile.close()
+    elif args.outputFile != None:
+        print "Result file requested, but no files found"
+        pass
+
+
+    datasets.download(dryrun=args.dryRun, clobber=False, nfiles=nfiles)
