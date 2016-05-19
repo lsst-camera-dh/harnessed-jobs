7dc5fa521559bc39bfc3d0931cd84c08198fe7e3
Homer A. Neal Jr <homer@rhel6-64k.slac.stanford.edu>
2016-05-19 15:08:19 -0700

    80% working now

--- T08/rebalive_functionality/v0/ccseorebalive_functionality.py
+++ T08/rebalive_functionality/v0/ccseorebalive_functionality.py
@@ -1,6 +1,12 @@
 ###############################################################################
 # REB aliveness functionality test
 #
+# Ex:
+# source ts8setup-test
+# [jh-test ts8prod@ts8-raft1 workdir]$ lcatr-harness --unit-type RTM --unit-id alive-test-1 --job rebalive_functionality --version v0
+#
+# author: homer    5/2016
+#
 ###############################################################################
 
 from org.lsst.ccs.scripting import *
@@ -17,19 +23,20 @@ if (True):
     pwrsub  = CCS.attachSubsystem("ccs-rebps");
     pwrmainsub  = CCS.attachSubsystem("ccs-rebps/MainPS");
 
+    ts8asp = [0,0,0,0,0,0,0,0]
     for i in range(5) :
-        ts8asp%i = CCS.attachSubsystem("ts8/R00.Reb2.ASPIC%d" % i)
+        ts8asp[i] = CCS.attachSubsystem("ts8/R00.Reb2.ASPIC%d" % i)
 
     time.sleep(3.)
 
     cdir = tsCWD
 
-    ts_version = ""
-    ts8_version = ""
-    ts_revision = ""
-    ts8_revision = ""
+    ts_version = "NA"
+    ts8_version = "NA"
+    ts_revision = "NA"
+    ts8_revision = "NA"
 
-    fp = open("%s/rebalive_report.results" % (cdir),"w");
+    fp = open("%s/rebalive_results.txt" % (cdir),"w");
 
 # attempt to power apply the REB power
     test_name = "apply_power"
@@ -46,14 +53,14 @@ if (True):
     try:
         result = ts8sub.synchCommand(10,"getREBDevices");
         rebs = result.getResult();
-        status_values = rebs
+        status_value = rebs
     except:
         status_value = "failed"
     fp.write("%-20s\t | \t %s\n" % (test_name,status_value));
 
     for rebid in rebs :
-        print "REB ID = %s" % rebid
-        print "=============================="
+        fp.write("\n\nREB ID = %s\n" % rebid)
+        fp.write("==============================\n")
 # Read 1-wire ID chip and record the value.
         test_name = "link_integrity"
         try:
@@ -78,13 +85,14 @@ if (True):
 
 
 # Read the voltage and current consumption measured by the VP5 LTC2945 current monitor on the REB, record the value and compare to the current measured at the P/S.
-        chans = [6V 9V 24V 40V]
+
+        chans = ["6V","9V","24V","40V"]
 
         for chn in chans :
             test_name = "VP5_LTC2945_%s" % chn
             try:
                 result = ts8sub.synchCommand(10,"getChannelValue D%s.%sv" % (rebid,chn));
-                rebv =  = result.getResult();
+                rebv = result.getResult();
                 result = ts8sub.synchCommand(10,"getChannelValue D%s.%si" % (rebid,chn));
                 rebi  = result.getResult();
                 fp.write("%-20s\t | \t %f \t %f  | \t %f \t %f \n" % (test_name,rebv, rebi, curmin[chn],curmax[chn]));
@@ -132,7 +140,7 @@ if (True):
             test_name = "VP5_LTC2945_%s" % chn
             try:
                 result = ts8sub.synchCommand(10,"getChannelValue D%s.%sv" % (rebid,chn));
-                rebv =  = result.getResult();
+                rebv = result.getResult();
                 result = ts8sub.synchCommand(10,"getChannelValue D%s.%si" % (rebid,chn));
                 rebi  = result.getResult();
                 fp.write("%-20s\t | \t %f \t %f  | \t %f \t %f \n" % (test_name,rebv, rebi, curmin[chn],curmax[chn]));
@@ -155,11 +163,22 @@ if (True):
 
 # Readback the temperatures measured by each of the ADT7420 sensors on the REB.
 
-        for itemp in range(1,5) :
-            test_name = "Temp_%s" % itemp
+        for itemp in range(1,9) :
+            test_name = "REB Temp_%s" % itemp
             try:
-                result = ts8sub.synchCommand(10,"getChannelValue %s.%d" % (rebid,itemp));
-                rebt =  = result.getResult();
+                cmnd = "getChannelValue %s.Temp%d" % (rebid,itemp)
+                print "temperature read command=%s" % cmnd
+                result = ts8sub.synchCommand(10,cmnd);
+                rebt = result.getResult();
+                fp.write("%-20s\t | \t %f \t %f  | \t %f \t %f \n" % (test_name,rebt));
+            except:
+                fp.write("%-20s\t | failed | failed \n" % (test_name));
+
+        for itemp in range(1,3) :
+            test_name = "DREB Temp_%s" % itemp
+            try:
+                result = ts8sub.synchCommand(10,"getChannelValue D%s.Temp%d" % (rebid,itemp));
+                rebt = result.getResult();
                 fp.write("%-20s\t | \t %f \t %f  | \t %f \t %f \n" % (test_name,rebt));
             except:
                 fp.write("%-20s\t | failed | failed \n" % (test_name));
@@ -175,7 +194,7 @@ if (True):
             test_name = "VP5_LTC2945_%s" % chn
             try:
                 result = ts8sub.synchCommand(10,"getChannelValue D%s.%sv" % (rebid,chn));
-                rebv =  = result.getResult();
+                rebv = result.getResult();
                 result = ts8sub.synchCommand(10,"getChannelValue D%s.%si" % (rebid,chn));
                 rebi  = result.getResult();
                 fp.write("%-20s\t | \t %f \t %f  | \t %f \t %f \n" % (test_name,rebv, rebi, curmin[chn],curmax[chn]));
@@ -185,34 +204,44 @@ if (True):
 
 #13. Configure the ASPICs to standard gain and RC time constant, and leave the inputs in clamped state.
 
-         for i in range(5) :
-             ts8asp%i.synchCommand(10,"change clamp 1");
+        for i in range(5) :
+            ts8asp[i].synchCommand(10,"change clamp 1");
 
 
 #14. Execute a zero-second exposure and readout sequence. Start a timer when the close shutter command executes.
  
-         ts8sub.synchCommand(10,"setHeader","TestType","LAMBDA",False)
-         ts8sub.synchCommand(10,"setHeader","ImageType","BIAS",False)
-         ts8sub.synchCommand(10,"setExposureParameter exposure_time 0");
-         ts8sub.synchCommand(10,"setExposureParameter open_shutter false");
-         ts8sub.synchCommand(10,"setFitsFilesOutputDirectory","%s" % (cdir));
+        ts8sub.synchCommand(10,"setHeader","TestType","LAMBDA",False)
+        ts8sub.synchCommand(10,"setHeader","ImageType","BIAS",False)
+        ts8sub.synchCommand(10,"setExposureParameter exposure_time 0");
+        ts8sub.synchCommand(10,"setExposureParameter open_shutter false");
+        ts8sub.synchCommand(10,"setFitsFilesOutputDirectory","%s" % (cdir));
+        
+        fitsfilename = "%s_lambda_bias_%4.4d_%3.3d_%s.fits" % (rebid,0,0,time.time())
+        ts8sub.synchCommand(10,"setFitsFilesNamePattern",fitsfilename);
+        print "fitsfilename = %s" % fitsfilename
+        
+        tm_start = time.time()
+        print "Ready to take image with exptime = %f at time = %f" % (0,tm_start)
+        
+        result = ts8sub.synchCommand(500,"exposeAcquireAndSave");
+        something = result.getResult();
+        tm_end = time.time()
+        print "done taking image with exptime = %f at time = %f" % (0,tm_end)
+        
+        fp.write("bias image\t | %s | %s \n" % (tm_start, tm_end));
 
-         fitsfilename = "%s_lambda_flat_%4.4d_%3.3d_%s.fits" % (raft,int(wl),seq,time.time())
-         ts8sub.synchCommand(10,"setFitsFilesNamePattern",fitsfilename);
-         print "fitsfilename = %s" % fitsfilename
 
-         tm_start = time.time()
-         print "Ready to take image with exptime = %f at time = %f" % (0,tm)
-       
-         result = ts8sub.synchCommand(500,"exposeAcquireAndSave");
-         something = result.getResult();
-         tm_end = time.time()
+    fp.close();
 
-         fp.write("bias image\t | %s | %s \n" % (tm_start, tm_end);
-
-
-     fp.close();
-
+# satisfy the expectations of ccsTools
+    istate=0;
+    fp = open("%s/status.out" % (cdir),"w");
+    fp.write(`istate`+"\n");
+    fp.write("%s\n" % ts_version);
+    fp.write("%s\n" % ts_revision);
+    fp.write("%s\n" % ts8_version);
+    fp.write("%s\n" % ts8_revision);
+    fp.close();
 
 
 print "rebalive_functionalty test END"
