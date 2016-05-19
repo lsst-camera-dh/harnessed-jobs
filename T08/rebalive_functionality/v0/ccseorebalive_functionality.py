a7be0addba927fb795b35cdf50d8f03594dd6242
Homer A. Neal Jr <homer@rhel6-64k.slac.stanford.edu>
2016-05-19 12:56:04 -0700

    prepped for testing

--- T08/rebalive_functionality/v0/ccseorebalive_functionality.py
+++ T08/rebalive_functionality/v0/ccseorebalive_functionality.py
@@ -17,6 +17,9 @@ if (True):
     pwrsub  = CCS.attachSubsystem("ccs-rebps");
     pwrmainsub  = CCS.attachSubsystem("ccs-rebps/MainPS");
 
+    for i in range(5) :
+        ts8asp%i = CCS.attachSubsystem("ts8/R00.Reb2.ASPIC%d" % i)
+
     time.sleep(3.)
 
     cdir = tsCWD
@@ -35,7 +38,7 @@ if (True):
         status_value = "success";
     except:
         status_value = "failed"
-    fp.write("%-20s\t | \t %s\n" % (test_name,status_value);
+    fp.write("%-20s\t | \t %s\n" % (test_name,status_value));
 
 #  Verify data link integrity.
     rebs = ""
@@ -46,7 +49,7 @@ if (True):
         status_values = rebs
     except:
         status_value = "failed"
-    fp.write("%-20s\t | \t %s\n" % (test_name,status_value);
+    fp.write("%-20s\t | \t %s\n" % (test_name,status_value));
 
     for rebid in rebs :
         print "REB ID = %s" % rebid
@@ -60,7 +63,7 @@ if (True):
             status_value = "success"
         except:
             status_value = "failed"
-        fp.write("%-20s\t | \t %s\n" % (test_name,status_value);
+        fp.write("%-20s\t | \t %s\n" % (test_name,status_value));
 
 # Read and record the REB firmware version.
 # see: https://confluence.slac.stanford.edu/display/LSSTCAM/REB+Register+Sets
@@ -71,22 +74,22 @@ if (True):
             status_value = "success"
         except:
             status_value = "failed"
-        fp.write("%-20s\t | \t %s\n" % (test_name,status_value);
+        fp.write("%-20s\t | \t %s\n" % (test_name,status_value));
 
 
 # Read the voltage and current consumption measured by the VP5 LTC2945 current monitor on the REB, record the value and compare to the current measured at the P/S.
         chans = [6V 9V 24V 40V]
+
         for chn in chans :
+            test_name = "VP5_LTC2945_%s" % chn
             try:
-                test_name = "VP5_LTC2945_%s_I" % chn
                 result = ts8sub.synchCommand(10,"getChannelValue D%s.%sv" % (rebid,chn));
                 rebv =  = result.getResult();
                 result = ts8sub.synchCommand(10,"getChannelValue D%s.%si" % (rebid,chn));
                 rebi  = result.getResult();
-                fp.write("%-20s\t | \t %f \t %f\n" % (test_name,rebv, rebi);
+                fp.write("%-20s\t | \t %f \t %f  | \t %f \t %f \n" % (test_name,rebv, rebi, curmin[chn],curmax[chn]));
             except:
-                test_name = "VP5_LTC2945"
-                status_value = "failed"
+                fp.write("%-20s\t | failed | failed \n" % (test_name));
 
 
 # Apply the analog power supply voltages (VP15_UNREG, VN15_UNREG, VP7_UNREG, VP40_UNREG) to the REB in the correct sequence (check with Rick for sequence and voltage values). Abort the test if any supply hits it overcurrent limit. Readback voltages and current consumption at the P/S and at the REB LTC2945 sensors.
@@ -97,14 +100,17 @@ if (True):
             status_value = "success";
         except:
             status_value = "failed"
-        fp.write("%-20s\t | \t %s\n" % (test_name,status_value);
+        fp.write("%-20s\t | \t %s\n" % (test_name,status_value));
+
+
+
         test_name = "apply analog power"
         try:
             result = pwrsub.synchCommand(10,"loadBiasDacs",true);
             status_value = "success";
         except:
             status_value = "failed"
-        fp.write("%-20s\t | \t %s\n" % (test_name,status_value);
+        fp.write("%-20s\t | \t %s\n" % (test_name,status_value));
 #
 # 
 #
@@ -123,16 +129,16 @@ if (True):
         curmax=dict({'6V':750.0, '9V':600.0, '24V':300, '40V':120}) 
 
         for chn in chans :
+            test_name = "VP5_LTC2945_%s" % chn
             try:
-                test_name = "VP5_LTC2945_%s_I" % chn
                 result = ts8sub.synchCommand(10,"getChannelValue D%s.%sv" % (rebid,chn));
                 rebv =  = result.getResult();
                 result = ts8sub.synchCommand(10,"getChannelValue D%s.%si" % (rebid,chn));
                 rebi  = result.getResult();
-                fp.write("%-20s\t | \t %f \t %f  | \t %f \t %f \n" % (test_name,rebv, rebi, curmin[chn],curmax[chn]);
+                fp.write("%-20s\t | \t %f \t %f  | \t %f \t %f \n" % (test_name,rebv, rebi, curmin[chn],curmax[chn]));
             except:
-                test_name = "VP5_LTC2945"
-                status_value = "failed"
+                fp.write("%-20s\t | failed | failed \n" % (test_name));
+
 
 
 
@@ -142,130 +148,73 @@ if (True):
             status_value = "success";
         except:
             status_value = "failed"
-        fp.write("%-20s\t | \t %s\n" % (test_name,status_value);
+        fp.write("%-20s\t | \t %s\n" % (test_name,status_value));
 
 
 
 
 # Readback the temperatures measured by each of the ADT7420 sensors on the REB.
 
+        for itemp in range(1,5) :
+            test_name = "Temp_%s" % itemp
+            try:
+                result = ts8sub.synchCommand(10,"getChannelValue %s.%d" % (rebid,itemp));
+                rebt =  = result.getResult();
+                fp.write("%-20s\t | \t %f \t %f  | \t %f \t %f \n" % (test_name,rebt));
+            except:
+                fp.write("%-20s\t | failed | failed \n" % (test_name));
 
 # Apply the CCD bias voltages and set the CCD clock rails (to E2V levels).
+        result = ts8sub.synchCommand(90,"loadSequencer","seq_1M.xml");
 
-ccs-rebps/setBiasDac ?  ccs-rafts loadNamedConfig, ccs-rafts loadDacs, ccs-rafts loadBiasDacs
+#ccs-rebps/setBiasDac ?  ccs-rafts loadNamedConfig, ccs-rafts loadDacs, ccs-rafts loadBiasDacs
 
-12. Re-check supply currents and verify they do not exceed expected values.
-13. Configure the ASPICs to standard gain and RC time constant, and leave the inputs in
-clamped state.
+#12. Re-check supply currents and verify they do not exceed expected values.
 
-ccs-rebps/sequencePower  ? ?  ccs-rafts loadAspics
-
-14. Execute a zero-second exposure and readout sequence. Start a timer when the \u201cclose
-shutter\u201d command executes.
-loadSequencer ...
-
-setExposureParameter open_shutter false
-setExposureParameter exposure_time 0
-exposeAcquireAndSave
-
-(I am purposely skipping the directory and filename settings)
-
-  
-    result = ts8sub.synchCommand(90,"loadSequencer",acffile);
-
-    raft = CCDID
-    print "Working on RAFT %s" % raft
-
-#
-# take bias images
-# 
-
-    ts8sub.synchCommand(10,"setExposureParameter open_shutter false");
-
-    print "setting location of bias fits directory"
-    ts8sub.synchCommand(10,"setFitsFilesOutputDirectory","%s" % (cdir));
-
-#            result = ts8sub.synchCommand(10,"setRaftName",raft)
-# Note: throws a not implemented exception
-
-    ts8sub.synchCommand(10,"setHeader","TestType","LAMBDA",False)
-    ts8sub.synchCommand(10,"setHeader","ImageType","BIAS",False)
-
-    for i in range(2):
-        timestamp = time.time()
-        ts8sub.synchCommand(10,"setExposureParameter exposure_time 0"); 
-
-        fitsfilename = "%s_lambda_bias_%3.3d_%s.fits" % (raft,seq,time.time())
-        ts8sub.synchCommand(10,"setFitsFilesNamePattern",fitsfilename);
-        
-        print "Ready to take bias image. time = %f" % time.time()
-        result = ts8sub.synchCommand(500,"exposeAcquireAndSave");
-        fitsfilename = result.getResult();
+        for chn in chans :
+            test_name = "VP5_LTC2945_%s" % chn
+            try:
+                result = ts8sub.synchCommand(10,"getChannelValue D%s.%sv" % (rebid,chn));
+                rebv =  = result.getResult();
+                result = ts8sub.synchCommand(10,"getChannelValue D%s.%si" % (rebid,chn));
+                rebi  = result.getResult();
+                fp.write("%-20s\t | \t %f \t %f  | \t %f \t %f \n" % (test_name,rebv, rebi, curmin[chn],curmax[chn]));
+            except:
+                fp.write("%-20s\t | failed | failed \n" % (test_name_));
 
 
+#13. Configure the ASPICs to standard gain and RC time constant, and leave the inputs in clamped state.
 
-# take light exposures
-#            ts8sub.synchCommand(10,"setExposureParameter","Light","1");
-            ts8sub.synchCommand(10,"setExposureParameter open_shutter true");
-
-            ts8sub.synchCommand(10,"setHeader","TestType","LAMBDA",False)
-            ts8sub.synchCommand(10,"setHeader","ImageType","FLAT",False)
+         for i in range(5) :
+             ts8asp%i.synchCommand(10,"change clamp 1");
 
 
-            print "setExposureParameter exposure_time %s" % str(int(exptime*1000))
-            ts8sub.synchCommand(10,"setExposureParameter exposure_time 10000");
+#ccs-rebps/sequencePower  ? ?  ccs-rafts loadAspics
 
-            for i in range(imcount):
-                print "image number = %d" % i
-# start acquisition
-                timestamp = time.time()
+#14. Execute a zero-second exposure and readout sequence. Start a timer when the close shutter command executes.
+ 
+         ts8sub.synchCommand(10,"setHeader","TestType","LAMBDA",False)
+         ts8sub.synchCommand(10,"setHeader","ImageType","BIAS",False)
+         ts8sub.synchCommand(10,"setExposureParameter exposure_time 0");
+         ts8sub.synchCommand(10,"setExposureParameter open_shutter false");
+         ts8sub.synchCommand(10,"setFitsFilesOutputDirectory","%s" % (cdir));
 
-                print "setExposureParameter exposure_time %s" % str(int(exptime*1000))
-                ts8sub.synchCommand(10,"setExposureParameter exposure_time 10000");
-                ts8sub.synchCommand(10,"setExposureParameter open_shutter true");
-                ts8sub.synchCommand(10,"setFitsFilesOutputDirectory","%s" % (cdir));
+         fitsfilename = "%s_lambda_flat_%4.4d_%3.3d_%s.fits" % (raft,int(wl),seq,time.time())
+         ts8sub.synchCommand(10,"setFitsFilesNamePattern",fitsfilename);
+         print "fitsfilename = %s" % fitsfilename
 
-                fitsfilename = "%s_lambda_flat_%4.4d_%3.3d_%s.fits" % (raft,int(wl),seq,time.time())
-                ts8sub.synchCommand(10,"setFitsFilesNamePattern",fitsfilename);
-                print "fitsfilename = %s" % fitsfilename
+         tm_start = time.time()
+         print "Ready to take image with exptime = %f at time = %f" % (0,tm)
+       
+         result = ts8sub.synchCommand(500,"exposeAcquireAndSave");
+         something = result.getResult();
+         tm_end = time.time()
 
-                print "Ready to take image with exptime = %f at time = %f" % (exptime,time.time())
-                ts8sub.synchCommand(500,"exposeAcquireAndSave");
-
-            seq = seq + 1
-
-    fpfiles.close();
-    fp.close();
-
-    fp = open("%s/status.out" % (cdir),"w");
-
-    istate=0;
-    result = tssub.synchCommandLine(10,"getstate");
-    istate=result.getResult();
-    fp.write(`istate`+"\n");
-    fp.write("%s\n" % ts_version);
-    fp.write("%s\n" % ts_revision);
-    fp.write("%s\n" % ts8_version);
-    fp.write("%s\n" % ts8_revision);
-    fp.close();
-
-try:
-    result = ts8sub.synchCommand(10,"setHeader","TestType","LAMBDA-END",False)
-    print "something"
-
-except Exception, ex:
+         fp.write("bias image\t | %s | %s \n" % (tm_start, tm_end);
 
 
-    raise Exception("There was an exception in the acquisition producer script. The message is\n (%s)\nPlease retry the step or contact an expert," % ex)
+     fp.close();
 
-except ScriptingTimeoutException, exx:
-
-    print "ScriptingTimeoutException at %f " % time.time()
-
-    result = pdsub.synchCommand(10,"softReset");
-    buff = result.getResult()
-
-    raise Exception("There was an exception in the acquisition producer script. The message is\n (%s)\nPlease retry the step or contact an expert," % exx)
 
 
 print "rebalive_functionalty test END"
