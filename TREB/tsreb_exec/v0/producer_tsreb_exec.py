c2c95032f8357ac71349c15f90de2a4810e54df1
Homer A. Neal Jr <homer@rhel6-64a.slac.stanford.edu>
2016-11-15 10:19:21 -0800

    initial tsreb exec jh scripts

--- TREB/tsreb_exec/v0/producer_tsreb_exec.py
+++ TREB/tsreb_exec/v0/producer_tsreb_exec.py
@@ -4,4 +4,25 @@ import sys
 import subprocess
 
 reb = os.environ["LCATR_UNIT_ID"]
-loc = "/nfs/farm/g/lsst/u1/jobHarness/jh_archive/REB/%s/vendorIngest/v0/" % reb
+
+class tsreb_test(object):
+    EXEC_TSREB = "source /opt/lsst/setup_tsreb;tsreb_wizard --%s" % reb_id
+    def __init__(self, reb_id, remote=None):
+        self.remote = remote  
+        self.reb_id = int(reb_id)
+        if (self.remote != None):
+            cmd = "ssh " + self.remote + " " + EXEC_TSREB
+        else:
+            cmd = EXEC_TSREB
+
+        data_lst = subprocess.check_output(cmd, shell=True)
+        data_lst = data_lst.strip()
+
+def do_test():
+    import lcatr.schema
+    reb_id = os.environ["LCATR_UNIT_ID"]
+    reb_id = reb_id.split("-")[1]
+    tsp = tsreb_test(reb_id, remote="tsreb@tsreb")
+
+if (__name__ == "__main__"):
+    do_test()
