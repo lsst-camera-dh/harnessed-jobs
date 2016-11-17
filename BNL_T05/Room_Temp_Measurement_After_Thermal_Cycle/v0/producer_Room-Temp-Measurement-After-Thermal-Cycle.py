ac19916688fdb6c4acb18fbf8b52502f008c6ef7
Homer A. Neal Jr <homer@rhel6-64a.slac.stanford.edu>
2016-11-16 21:41:38 -0800

    fixed .py file ref in producer

--- BNL_T05/Room_Temp_Measurement_After_Thermal_Cycle/v0/producer_Room-Temp-Measurement-After-Thermal-Cycle.py
+++ BNL_T05/Room_Temp_Measurement_After_Thermal_Cycle/v0/producer_Room-Temp-Measurement-After-Thermal-Cycle.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 from ccsTools import ccsProducer
 
-ccsProducer('Room-Temp-Measurement-After-Thermal-Cycle', 'Room-Temp-Measurement-After-Thermal-Cycle')
+ccsProducer('Room-Temp-Measurement-After-Thermal-Cycle', 'Room-Temp-Measurement-After-Thermal-Cycle.py')
