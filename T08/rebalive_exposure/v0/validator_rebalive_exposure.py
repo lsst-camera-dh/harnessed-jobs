0605fb3fd61ce8f377257466b8d9441580d93d04
Homer A. Neal Jr <homer@rhel6-64i.slac.stanford.edu>
2017-03-27 13:57:08 -0700

    added Paul's unresponsive channel checker to the rebalive exposure and added option of displaying image

--- T08/rebalive_exposure/v0/validator_rebalive_exposure.py
+++ T08/rebalive_exposure/v0/validator_rebalive_exposure.py
@@ -5,6 +5,8 @@ import siteUtils
 import shutil
 import lcatr.schema
 import glob
+import pyfits
+import sys
 
 jobDir = siteUtils.getJobDir()
 
@@ -15,6 +17,113 @@ shutil.copy("%s/plotchans.list" % jobDir ,os.getcwd())
 os.system("./rebalive_plots.sh > logpl &")
 
 jobName = "rebalive_exposure"
+
+
+# open images in rebalive_exposure, check for response to light
+# POC 20170316
+
+#import /astro/u/poc1/TS8_Commissioning/Python/boxes.py
+
+
+def boxesmean(d, BOXSZ=100, NUMBOXES=500, ximg=[10,522], yimg=[1,2002]):
+    """ compute mean of 500 ROI and return median -- for avoiding cosmics, defects etc """
+    yo = randint(yimg[0], yimg[1], NUMBOXES)
+    xo = randint(ximg[0], ximg[1], NUMBOXES)
+    return  median([d[yo[i]:yo[i]+BOXSZ,xo[i]:xo[i]+BOXSZ].mean() for i in range(NUMBOXES)])
+
+
+YIMG = [10,1990]  # rows in image and overscan areas
+XIMG = [10,500]   # columns in image areas
+OIMG = [515,570]  # columns in overscan areas
+
+# connectivity test image filenames used
+fl0=sorted(glob.glob('*alive_test*fits'))
+fl1=sorted(glob.glob('*1000ms*fits'))
+fl4=sorted(glob.glob('*4000ms*fits'))
+
+# make list of slots, segments, and amps
+slots = []
+for f in fl0:
+    slots.append([f[:2]] * 16)
+slots = slots[0] + slots[1] + slots[2] + slots[3] + slots[4] + slots[5] + slots[6] + slots[7] + slots[8]
+
+segments = ['10','11','12','13','14','15','16','17','07','06','05','04','03','02','01','00'] * 9
+
+amps = range(1,17)*9
+
+i0 = array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=XIMG) for f in fl0 for i in range(1,17)])
+
+o0 = array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=OIMG) for f in fl0 for i in range(1,17)])
+
+sig0 = i0 - o0
+
+i1 = array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=XIMG) for f in fl1 for i in range(1,17)])
+
+o1 = array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=OIMG) for f in fl1 for i in range(1,17)])
+
+sig1 = i1 - o1
+
+i4 = array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=XIMG) for f in fl4 for i in range(1,17)])
+
+o4 = array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=OIMG) for f in fl4 for i in range(1,17)])
+
+sig4 = i4 - o4
+
+# now search for non-responsive channels
+bad0 = find(sig0 < median(sig0)/10.)
+bad1 = find(sig1 < median(sig1)/10.)
+bad4 = find(sig4 < median(sig4)/10.)
+
+print("0 sec: (bad slot,bad segment, bad amp), signal " , [(slots[x],segments[x],amps[x])  for x in bad0], sig0[bad0])
+print("1 sec: (bad slot,bad segment, bad amp), signal " , [(slots[x],segments[x],amps[x])  for x in bad1], sig1[bad1])
+print("4 sec: (bad slot,bad segment, bad amp), signal " , [(slots[x],segments[x],amps[x])  for x in bad4], sig4[bad4])
+
+ofile = open('/tmp/conntest', 'w')
+print >> ofile, ("0 sec: (bad slot,bad segment, bad amp), signal " , [(slots[x],segments[x],amps[x])  for x in bad0], sig0[bad0])
+print >> ofile, ("1 sec: (bad slot,bad segment, bad amp), signal " , [(slots[x],segments[x],amps[x])  for x in bad1], sig1[bad1])
+print >> ofile, ("4 sec: (bad slot,bad segment, bad amp), signal " , [(slots[x],segments[x],amps[x])  for x in bad4], sig4[bad4])
+ofile.close()
+
+os.system('cat /tmp/conntest >> rebalive_results.txt')
+
+
+if (not "ready" in jobDir) :
+    for i in range(20):
+         print "This image viewing may be skipped. Skip the step (N/y)?"
+    sys.stdout.flush()
+    answer = raw_input("\n\nThis image viewing may be skipped. Skip the step (N/y)? \n\n")
+    if "y" in answer.lower() :
+         print "Operator requested to skip the step. BYE"
+    else :
+    
+    
+        apptxt = "Please check the image that is about to be projected in ds9"
+        
+        print apptxt
+        topq = Tkinter.Tk()
+        q = Tkinter.Button(topq, text = apptxt, command = topq.destroy, bg = "yellow", font = ("Helvetica",24))
+        q.pack()
+        topq.title('FLAT image check')
+        topq.mainloop()
+        
+        qefiles = sorted(glob.glob('*flat*.fits'))
+        if len(qefiles)>0 :
+             os.system("ds9 -mosaicimage iraf -scale datasec no -scale zscale -lock frame image *flat*fits")
+        
+
+
+
+
+
+
+for i in range(20) :
+     print "RETEST (N/y)?"
+sys.stdout.flush()
+answer = raw_input("RETEST (N/y)?")
+if "y" in answer.lower() :
+     raise Exception("PURPOSELY crashing to allow a retest via retrying the e-Traveler step")
+
+
 
 results = []
 
