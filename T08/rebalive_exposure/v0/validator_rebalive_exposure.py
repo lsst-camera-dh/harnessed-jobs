ef1e05c65cc20a3c6268f39db938da0630e51d7e
Homer A. Neal Jr <homer@rhel6-64i.slac.stanford.edu>
2017-03-31 07:18:14 -0700

    fixed image stats section

--- T08/rebalive_exposure/v0/validator_rebalive_exposure.py
+++ T08/rebalive_exposure/v0/validator_rebalive_exposure.py
@@ -7,6 +7,8 @@ import lcatr.schema
 import glob
 import pyfits
 import sys
+import numpy
+import numpy.random
 
 jobDir = siteUtils.getJobDir()
 
@@ -27,9 +29,9 @@ jobName = "rebalive_exposure"
 
 def boxesmean(d, BOXSZ=100, NUMBOXES=500, ximg=[10,522], yimg=[1,2002]):
     """ compute mean of 500 ROI and return median -- for avoiding cosmics, defects etc """
-    yo = randint(yimg[0], yimg[1], NUMBOXES)
-    xo = randint(ximg[0], ximg[1], NUMBOXES)
-    return  median([d[yo[i]:yo[i]+BOXSZ,xo[i]:xo[i]+BOXSZ].mean() for i in range(NUMBOXES)])
+    yo = numpy.random.randint(yimg[0], yimg[1], NUMBOXES)
+    xo = numpy.random.randint(ximg[0], ximg[1], NUMBOXES)
+    return  numpy.median([d[yo[i]:yo[i]+BOXSZ,xo[i]:xo[i]+BOXSZ].mean() for i in range(NUMBOXES)])
 
 
 YIMG = [10,1990]  # rows in image and overscan areas
@@ -50,29 +52,30 @@ slots = slots[0] + slots[1] + slots[2] + slots[3] + slots[4] + slots[5] + slots[
 segments = ['10','11','12','13','14','15','16','17','07','06','05','04','03','02','01','00'] * 9
 
 amps = range(1,17)*9
+#print "fl0=",fl0
 
-i0 = array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=XIMG) for f in fl0 for i in range(1,17)])
+i0 = numpy.array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=XIMG) for f in fl0 for i in range(1,17)])
 
-o0 = array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=OIMG) for f in fl0 for i in range(1,17)])
+o0 = numpy.array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=OIMG) for f in fl0 for i in range(1,17)])
 
 sig0 = i0 - o0
 
-i1 = array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=XIMG) for f in fl1 for i in range(1,17)])
+i1 = numpy.array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=XIMG) for f in fl1 for i in range(1,17)])
 
-o1 = array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=OIMG) for f in fl1 for i in range(1,17)])
+o1 = numpy.array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=OIMG) for f in fl1 for i in range(1,17)])
 
 sig1 = i1 - o1
 
-i4 = array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=XIMG) for f in fl4 for i in range(1,17)])
+i4 = numpy.array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=XIMG) for f in fl4 for i in range(1,17)])
 
-o4 = array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=OIMG) for f in fl4 for i in range(1,17)])
+o4 = numpy.array([boxesmean(pyfits.getdata(f,i), BOXSZ=10, NUMBOXES=50, yimg=YIMG, ximg=OIMG) for f in fl4 for i in range(1,17)])
 
 sig4 = i4 - o4
 
 # now search for non-responsive channels
-bad0 = find(sig0 < median(sig0)/10.)
-bad1 = find(sig1 < median(sig1)/10.)
-bad4 = find(sig4 < median(sig4)/10.)
+bad0 = numpy.where(sig0 < numpy.median(sig0)/10.)[0]
+bad1 = numpy.where(sig1 < numpy.median(sig1)/10.)[0]
+bad4 = numpy.where(sig4 < numpy.median(sig4)/10.)[0]
 
 print("0 sec: (bad slot,bad segment, bad amp), signal " , [(slots[x],segments[x],amps[x])  for x in bad0], sig0[bad0])
 print("1 sec: (bad slot,bad segment, bad amp), signal " , [(slots[x],segments[x],amps[x])  for x in bad1], sig1[bad1])
