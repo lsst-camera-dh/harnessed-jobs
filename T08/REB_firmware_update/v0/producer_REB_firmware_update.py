3c4cb897fcdd3f81a5c211459bba6c51ecf143b1
Homer A. Neal Jr <homer@rhel6-64i.slac.stanford.edu>
2017-05-16 08:03:47 -0700

    updated to find the most recent firmware file

--- T08/REB_firmware_update/v0/producer_REB_firmware_update.py
+++ T08/REB_firmware_update/v0/producer_REB_firmware_update.py
@@ -21,19 +21,23 @@ for i in range(10):
     top.title('Ready to start firmware update?')
     top.mainloop()
 
-    os.system('source et_prog_flash.sh REB_v5_top_30325002.mcs')
+    #get list of files in directory
+    list_of_firmware_files = glob.glob('/path/to/directory/*.mcs')
+    #find most recent file
+    recent_file = max(list_of_firmware_files, key=os.path.getctime)
+    
+    #call Stefano's REB5 firmware upgrade program
+    subprocess.call(['source et_prog_flash.sh', '/path/to/directory/' + recent_file])
 
-
-    print "Rebooting the RCE after a 5s wait"
-    time.sleep(5.0)
-    sout = subprocess.check_output("$HOME/rebootrce.sh", shell=True)
-    print sout
-    time.sleep(3.0)
+    #Stefano's software should reboot RCE automaticaly
+    #print "Rebooting the RCE after a 5s wait"
+    #time.sleep(5.0)
+    #sout = subprocess.check_output("$HOME/rebootrce.sh", shell=True)
+    #print sout
+    #time.sleep(3.0)
     
     print "Update another REB (Y/N)?"
     sys.stdout.flush()
     answer = raw_input("\n\nUpdate another REB (Y/N)?")
     if not "y" in answer.lower() :
         break
-
-
